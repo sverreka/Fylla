5a35c2a52ce6c7dcbde1c9afb477e87a1b25d4fa
sverrekaldestad <104861156+sverrekaldestad@users.noreply.github.com>
2024-11-18 19:33:44 +0100

    tweaks

--- mer/lib/main.dart
+++ mer/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fylla!',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       debugShowCheckedModeBanner: false,
       home: SafeArea(
